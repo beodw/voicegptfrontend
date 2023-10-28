fe7fbb9c54635b291fe6d158d4f9442ffd9a25f1
beodwilson <beodwilson@gmail.com>
2023-10-28 20:35:08 +0100

    add submission for further thoughts

--- src/components/surveys/RegularSurvey.js
+++ src/components/surveys/RegularSurvey.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState, Fragment } from 'react'
 import { retrieveCookie } from '../../lib/session';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
@@ -16,6 +16,7 @@ function RegularSurvey() {
     const [furtherAdditionalThoughts, setFurtherAdditionalThoughts] = useState("");
     const [isSurveyAlreadyFilled, setIsSurveyAlreadyFilled] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const [furtherAdditionalThoughtsSubmitted, setFurtherAdditionalThoughtsSubmitted] = useState(false);
     const dispatch = useDispatch();
     const closeSurvey = ()=>{
         dispatch(toggleSurvey(false))
@@ -25,7 +26,10 @@ function RegularSurvey() {
         setAdditionalThoughts(value)
     }
 
-    const updateFurtherAdditionalThoughts = () => {}
+    const updateFurtherAdditionalThoughts = (e) => {
+        const {value} = e.target;
+        setFurtherAdditionalThoughts(value);
+    }
 
     const rateQuestion = (value, index) => {
         questions[index].rating = value
@@ -74,7 +78,36 @@ function RegularSurvey() {
         });
     }
 
-    const submitFurtherFeedBack = () =>  {}
+    const submitFurtherFeedBack = () =>  {
+        if(furtherAdditionalThoughts.length === 0) return;
+        const oauthCode = retrieveCookie("voiceGPTAuthToken");
+        const payLoad = {oauthCode, furtherAdditionalThoughts}
+        setIsSubmitting(true)
+        fetch("https://ycwfx5u6r5bjnzvnsuv2rnpgmi0sosyb.lambda-url.eu-west-2.on.aws/ ",{method: "POST", body:JSON.stringify(payLoad)})
+         .then(r => {
+            if (r.ok) r.json()
+            .then(data => 
+                    {
+                        if(data.status == 200) setFurtherAdditionalThoughtsSubmitted(true)
+                        if(data.status == 400) {
+                            setIsSurveyAlreadyFilled(true)
+                            setIsFormComplete(true)
+                        }
+                        setIsSubmitting(false)
+                    }
+            )
+            .catch(e=>{
+                console.log(e)
+                alert("Could not submit further feedback.")
+                setIsSubmitting(false)
+            })
+        }) 
+         .catch(e => {
+            console.error(e)
+            alert("Could not reach endpoint for further feedback.")
+            setIsSubmitting(false)
+        });
+    }
 
   return <div className='h-screen w-full flex items-center justify-center'>
         <div className={`rounded-xl bg-[#202123] flex flex-col items-center shadow-lg ${isFormComplete ? isSurveyAlreadyFilled? 'h-1/2 px-6 w-1/2' : 'h-1/2 w-1/2' :'h-5/6 max-h-[800px]'} py-3`}>
@@ -86,20 +119,44 @@ function RegularSurvey() {
             {isFormComplete ?
                 isSurveyAlreadyFilled ? 
                 <>
-                    <h1 className='text-gray-300 font-bold text-xs xs:text-sm md:text-lg lg:text-xl text-justify'>
-                        It appears you have already left feedback. However, feel free to fill out the box below if you have additional thoughts.
-                    </h1>
-                    <textarea maxLength={100} onChange={updateFurtherAdditionalThoughts} value={furtherAdditionalThoughts} className='bg-gray-100 w-full rounded-lg mt-6 text-black p-2 text-sm outline-none'/>
-                    <div className='text-white flex w-full mt-6'>
-                        <button onClick={submitFurtherFeedBack} className='bg-green-500 rounded-lg text-xs xs:text-sm md:text-lg lg:text-xl py-2 px-4 xs:px-6 '>Submit</button>
-                    </div>
+                    {
+                        !furtherAdditionalThoughtsSubmitted ? (
+                            <>
+                                <h1 className='text-gray-300 font-bold text-xs xs:text-sm md:text-lg lg:text-xl text-justify'>
+                                    It appears you have already left feedback. However, feel free to fill out the box below if you have additional thoughts.
+                                </h1>
+                                <textarea maxLength={100} onChange={updateFurtherAdditionalThoughts} value={furtherAdditionalThoughts} className='bg-gray-100 w-full rounded-lg mt-6 text-black p-2 text-sm outline-none'/>
+                                <div className='text-white flex w-full mt-6'>
+                                    <button onClick={submitFurtherFeedBack} className='bg-green-500 rounded-lg text-xs xs:text-sm md:text-lg lg:text-xl py-2 px-4 xs:px-6 '>
+                                        {isSubmitting ? "Submitting" : "Submit"}
+                                    </button>
+                                </div>
+                            </>
+                        )
+                        :
+                        (
+                            <>
+                                <h1 className='text-gray-300 mt-4 font-bold text-xs xs:text-sm md:text-lg lg:text-xl text-justify'>
+                                    Thanks for taking the time to give us your feedback!
+                                </h1>
+                                <div className='h-[140px] sm:h-1/2'></div>
+                                <button onClick={closeSurvey} className='bg-gray-500 text-white rounded-lg text-xs xs:text-sm md:text-lg lg:text-xl py-2 px-4 xs:px-6 '>
+                                        {"Close"}
+                                </button>
+                            </>
+                        )
+                    }
                 </> :
                 <>
-                    <h1 className='text-gray-300 font-bold text-xs xs:text-sm md:text-lg lg:text-xl'>Congratulations!</h1>
-                    <h1 className='text-gray-300 mt-2 text-xs xs:text-sm md:text-lg'>You have won an Amazon Gift Card.</h1>
-                    <div className='rounded-lg p-2 w-1/2 border border-gray-500 flex items-center justify-center overflow-hidden mt-4'>
+                    <h1 className='text-gray-300 font-bold text-xs xs:text-sm md:text-lg lg:text-xl'>Stand a chance to win!</h1>
+                    <p className='text-gray-300 mt-2 text-xs xs:text-sm md:text-lg'>Congratulations! Please enter your email to registered in the draw.<br/>We will notiify you if you win!</p>
+                    <input type='email' placeholder='e.g. beodwilson@gmail.com'/>
+                    {/* <div className='rounded-lg p-2 w-1/2 border border-gray-500 flex items-center justify-center overflow-hidden mt-4'>
                         <h1 className='text-gray-300 text-xs xs:text-sm md:text-lg'>Code: SDKS787*&*SDSlksaj</h1>
-                    </div>
+                    </div> */}
+                     <button onClick={closeSurvey} className='bg-green-500 rounded-lg text-xs xs:text-sm md:text-lg lg:text-xl py-2 px-4 xs:px-6 '>
+                            {"Submit"}
+                    </button>
                 </>
                 :
                 <>
@@ -109,22 +166,22 @@ function RegularSurvey() {
                         {
                             questions.map(
                                     (q, index) => (
-                                        <>
+                                        <Fragment key={index}>
                                             <p key={index} className='w-full text-justify text-xs xs:text-sm md:text-lg lg:text-xl'>{index+1}. {q.question}</p>
-                                            <div key={(index+1)*2} className='flex justify-between w-full'>
+                                            <div key={`${index}ratingContainer`} className='flex justify-between w-full'>
                                                 {
                                                     [1,2,3,4,5].map(
-                                                        (v, i) => <button onClick={() => rateQuestion(v,index)} key={i} className={`hover:bg-gray-500 ${q.rating == i+1 && 'bg-gray-500'} border border-gray-100 flex justify-center items-center rounded-lg p-2 text-xs md:text-lg w-[35px] h-[35px] xs:w-10 xs:h-10 md:w-12 md:h-12 `}>{v}</button>
+                                                        (v, i) => <button onClick={() => rateQuestion(v,index)} key={`${index}:${i}`} className={`hover:bg-gray-500 ${q.rating == i+1 && 'bg-gray-500'} border border-gray-100 flex justify-center items-center rounded-lg p-2 text-xs md:text-lg w-[35px] h-[35px] xs:w-10 xs:h-10 md:w-12 md:h-12 `}>{v}</button>
                                                     )
                                                 }
                                             </div>
-                                        </>
+                                        </Fragment>
                                     )
                             )
                         }
                         <p className='text-gray-300 w-full text-justify text-xs xs:text-sm md:text-lg lg:text-xl'>{questions.length+1}. Any additional thoughts?{" (Optional)"}</p>
                         <div className='text-gray-300 flex w-full'>
-                            <textarea maxLength={100} onChange={updateAdditionalThoughts} value={additionalThoughts} className='bg-gray-100 w-full rounded-lg text-black p-2 text-sm outline-none'/>
+                            <textarea maxLength={5000} onChange={updateAdditionalThoughts} value={additionalThoughts} className='bg-gray-100 w-full rounded-lg text-black p-2 text-sm outline-none'/>
                         </div>
                     </div>
                     <div className='text-white flex w-full mt-6 px-10 sm:px-12'>
